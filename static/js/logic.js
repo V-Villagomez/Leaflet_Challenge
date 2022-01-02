8768b3196d8c14dc62311b3ee1ccfc9af3352f3e
Vanessa Villagomez <villagomez.v@outlook.com>
2022-01-01 17:41:33 -0800

    update file

--- static/js/logic.js
+++ static/js/logic.js
@@ -5,10 +5,29 @@ var myMap = L.map("map", {
   });
 
 // Adding the tile layer
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+L.tileLayer(
+    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+    {
+      attribution:
+        "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+      tileSize: 512,
+      maxZoom: 18,
+      zoomOffset: -1,
+      id: "mapbox/light-v10",
+      accessToken: API_KEY
 }).addTo(myMap);
 
-// Load url data
+// Connect to geojson using D3
 var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+d3.json(url, function(data) {
+    // Create functions: markers with size corresponding to quake mag by their size and depth by color (higher mag appear larger and greater depth appear darker in color)
+    
+    
+    // Create popups that provide add'l info about the quake when a marker is clicked
+    
+    
+    // Create a legend that will provide context for map data (data point)
+
+  
+});
 
