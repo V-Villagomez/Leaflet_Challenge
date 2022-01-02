3d6a376b0643d56bd5b031048123e8a3fffbbd2a
Vanessa Villagomez <villagomez.v@outlook.com>
2022-01-01 18:00:21 -0800

    update file

--- static/js/logic.js
+++ static/js/logic.js
@@ -21,7 +21,24 @@ L.tileLayer(
 var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 d3.json(url, function(data) {
     // Create functions: markers with size corresponding to quake mag by their size and depth by color (higher mag appear larger and greater depth appear darker in color)
+    for (var i = 0; i < data.features.length; i++) {
+        
+        // Excerpt coordinates, magnitude,location name and date of earthquakes
+        var coord = data.features[i].geometry.coordinates;
+        var mag = data.features[i].properties.mag;
+        var loc_name = data.features[i].properties.place;
+        var date = new Date(data.features[i].properties.time);
     
+    // Circle markers
+    L.circle([coord[1], coord[0]], {
+        color: "#E3E3E3",
+        weight: 1,
+        fillColor: getColor(mag),
+        fillOpacity: 0.5,
+        radius: mag * 50000 // Adjust radius size
+    }).bindPopup("<h1>" + loc_name + "</h1><hr><h3> Magnitude: " + mag + "</h3>" + "<p>" + date + "</p>") // Add tooltip
+    .addTo(myMap);}
+
     
     // Create popups that provide add'l info about the quake when a marker is clicked
     
