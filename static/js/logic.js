97338ef78a35b4452e6777b7cfa4ec2458cff215
Vanessa Villagomez <villagomez.v@outlook.com>
2022-01-01 21:47:40 -0800

    finalize file

--- static/js/logic.js
+++ static/js/logic.js
@@ -34,27 +34,14 @@ function getColor(mag) {
             return "greenyellow"
     }
 }
-function getRadius(mag) {
-    switch(true) {
-        case mag > 90:
-            return 14;
-            break;
-        case mag > 70:
-            return 12;
-            break;
-        case mag > 50:
-            return 10;
-        case mag > 30:
-            return 8;
-            break;
-        case mag > 10:
-            return 6;
-            break
-        default:
-            return 4;
-            break;
+
+// Adjust radius with a mag of 0
+function getRadius(magnitude) {
+    if (magnitude === 0) {
+      return 1;
     }
-}
+    return magnitude * 4;
+  }
 
 // Connect to geojson using D3
 var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
@@ -76,47 +63,6 @@ d3.json(url).then(function(data) {
         + new Date(feature.properties.time) + "</p><hr><p>Magnitude: " + feature.properties.mag + "</p>");
     }
 }).addTo(myMap);
-
-// // Create functions: markers with size corresponding to quake mag by their size and depth by color (higher mag appear larger and greater depth appear darker in color)
-//     function markerSize(mag) {
-//         return mag * 4;
-//     };
-
-    // function getColor(depth) {
-    //     switch(true) {
-    //         case depth > 90:
-    //             return "red";
-    //         case depth > 70:
-    //             return "orangered";
-    //         case depth > 50:
-    //             return "orange";
-    //         case depth > 30:
-    //             return "goldenrod"
-    //         case depth > 10:
-    //             return "yellow";
-    //         default:
-    //             return "greenyellow"
-    //     }
-    // }
-
-// L.geoJSON(data, {
-//     pointToLayer: function(feature, latlng) {
-//         return L.circleMarker(latlng, {
-//             radius: markerSize(feature.properties.mag),
-//             fillColor: getColor(feature.geometry.coordinates[2]),
-//             fillOpacity: 0.7,
-//             color: "black",
-//             stroke: true,
-//             weight: 0.5
-//         }
-//     );
-// },
-// // Create popups that provide add'l info about the quake when a marker is clicked
-// onEachFeature: function(feature, layer) {
-//     layer.bindPopup("<h3>Location: " + feature.properties.place + "</h3><hr><p>Date: "
-//     + new Date(feature.properties.time) + "</p><hr><p>Magnitude: " + feature.properties.mag + "</p>");
-//   }
-
 
 // Create a legend that will provide context for map data (data point)
 var legend = L.control({position: "bottomright"});
