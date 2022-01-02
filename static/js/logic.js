d40dbc6a91fb2d1f54f87e60ce4f18b0e65301c9
Vanessa Villagomez <villagomez.v@outlook.com>
2022-01-01 19:49:14 -0800

    udpate file

--- static/js/logic.js
+++ static/js/logic.js
@@ -20,6 +20,7 @@ quakeMap.addTo(myMap);
 // Connect to geojson using D3
 var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 d3.json(url, function(data) {
+    //console.log(data)
 // Create functions: markers with size corresponding to quake mag by their size and depth by color (higher mag appear larger and greater depth appear darker in color)
     function markerSize(mag) {
         return mag * 4;
@@ -40,17 +41,29 @@ d3.json(url, function(data) {
                 return "greenyellow"
         }
     }
-})
-
-
-
+L.geoJSON(data, {
+    pointToLayer: function(feature, latlng) {
+        return L.circleMarker(latlng, {
+            radius: markerSize(feature.properties.mag),
+            fillColor: getColor(feature.geometry.coordinates[2]),
+            fillOpacity: 0.7,
+            color: "black",
+            stroke: true,
+            weight: 0.5
+        }
+    );
+},
 // Create popups that provide add'l info about the quake when a marker is clicked
-        
+onEachFeature: function(feature, layer) {
+    layer.bindPopup("<h3>Location: " + feature.properties.place + "</h3><hr><p>Date: "
+    + new Date(feature.properties.time) + "</p><hr><p>Magnitude: " + feature.properties.mag + "</p>");
+  }
+}).addTo(myMap);
 
 // Create a legend that will provide context for map data (data point)
     
 
 //legend.addTo(myMap);
   
-//});
+});
 
