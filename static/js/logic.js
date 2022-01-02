671255f8058a70e02c184936d456deb9b157ceac
Vanessa Villagomez <villagomez.v@outlook.com>
2022-01-01 19:26:47 -0800

    update functions

--- static/js/logic.js
+++ static/js/logic.js
@@ -19,8 +19,29 @@ quakeMap.addTo(myMap);
 
 // Connect to geojson using D3
 var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
-
+d3.json(url, function(data) {
 // Create functions: markers with size corresponding to quake mag by their size and depth by color (higher mag appear larger and greater depth appear darker in color)
+    function markerSize(mag) {
+        return mag * 4;
+    };
+    function getColor(depth) {
+        switch(true) {
+            case depth > 90:
+                return "red";
+            case depth > 70:
+                return "orangered";
+            case depth > 50:
+                return "orange";
+            case depth > 30:
+                return "goldenrod"
+            case depth > 10:
+                return "yellow";
+            default:
+                return "greenyellow"
+        }
+    }
+})
+
 
 
 // Create popups that provide add'l info about the quake when a marker is clicked
